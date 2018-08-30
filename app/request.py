310d910daa67b5ecb3cfd43011bbc8ab21808ad6
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-30 16:44:12 +0300

    created a function that gets the json response

--- app/request.py
+++ app/request.py
@@ -1,2 +1,24 @@
 from app import app
-api_key = app.config['NEWS_API_BASE_URL']
+import urllib.request,json
+from .models import news
+
+News = news.News
+
+api_key = app.config['NEWS_API_KEY']
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_news(category):
+        '''
+        Function that gets json response to url request
+        '''
+        get_news_url = base_url.format(category, api_key)
+
+        with urllib.request.urlopen(get_news_url) as url:
+            get_news_data = url.read()
+            get_news_response = json.loads(get_news_data)
+
+            news_results = None
+
+            if get_news_response['results']:
+                news_results_list = get_news_response['results']
+                news_results = process_results(news_results_list)
