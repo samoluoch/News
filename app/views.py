36a8db7f6cbf5caf361824c8b9e0ea933764f819
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-08-30 12:12:07 +0300

    defined the function in the views that allows the display of the indx.html page

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    '''
+    A function for the root page that will return the index page and its data
+    '''
+    return render_template(index.html)
