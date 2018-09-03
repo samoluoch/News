6fb833e3e6cb2d7bff7e1764a1abc20b064ea30e
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-03 08:42:10 +0300

    fetched the news headlines in the news structure

--- app/request.py
+++ app/request.py
@@ -8,7 +8,8 @@ base_url = None
 def configure_request(app):
     global api_key,base_url
     api_key = app.config['NEWS_API_KEY']
-    base_url = app.config["NEWS_API_BASE_URL"]
+    base_url = app.config['NEWS_API_BASE_URL']
+    
     
 
 def get_news(category):
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     '''
     NEWS_API_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     pass
 
